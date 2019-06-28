cb4e3c2494a48e3495bc2396d7d04e56c4944c41
c-s-n <development@p4p3.net>
2019-06-28 11:14:47 +0200

    Add baustellen OSM and converter from node to way tags
    Rename converter solution

---
+++ backend/import/DotNetCoreConverters/NodeToWayConverter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NodeToWayConverter
+{
+    /// <summary>
+    /// Program to read an OpenStreetMap XML with nodes plus tags only and add a way for each node
+    /// with a defined tag from the nodes.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine($"Usage: {Process.GetCurrentProcess().MainModule.FileName} {Path.GetFileName(Assembly.GetEntryAssembly().Location)} <path to OSM file> <path to output directory> <tag to take over>");
+                return;
+            }
+
+            var sourceFile = new FileInfo(args[0]);
+            var outputDirectory = new DirectoryInfo(args[1]);
+
+            if (!sourceFile.Exists)
+            {
+                Console.Error.WriteLine("Source file does not exist");
+            }
+
+            if (sourceFile.Directory.FullName.Equals(outputDirectory.FullName))
+            {
+                Console.Error.WriteLine("Source and output directory must not be the same");
+                return;
+            }
+
+            if (!outputDirectory.Exists)
+            {
+                outputDirectory.Create();
+            }
+
+            ProcessFile(sourceFile, outputDirectory, args[2]);
+        }
+
+        /// <summary>
+        /// Processes the <paramref name="sourceFile"/>. Generates an output file with the desired structure.
+        /// </summary>
+        /// <param name="sourceFile">The source file.</param>
+        /// <param name="outputDirectory">The existing output directory.</param>
+        /// <param name="tagKey">The key of the tag to use.</param>
+        private static void ProcessFile(FileInfo sourceFile, DirectoryInfo outputDirectory, string tagKey)
+        {
+            var outputFile = new FileInfo(Path.Combine(outputDirectory.FullName, sourceFile.Name));
+
+            using (var readStream = sourceFile.OpenRead())
+            using (var writeStream = outputFile.Open(FileMode.Truncate))
+            using (var xmlWriter = XmlWriter.Create(writeStream, new XmlWriterSettings { Indent = true }))
+            {
+                var sourceDocument = new XmlDocument();
+                sourceDocument.Load(readStream);
+                var osm = sourceDocument.DocumentElement;
+
+                var targetRoot = new XElement("osm", new XAttribute("version", "0.6"));
+
+                foreach (XmlNode node in osm.GetElementsByTagName("node"))
+                {
+                    var tagValue = node.SelectSingleNode($"tag[@k='{tagKey}']")?.Attributes.GetNamedItem("v")?.Value;
+                    if (tagValue != null)
+                    {
+                        var nodeId = node.Attributes.GetNamedItem("id").Value;
+                        var targetNode = new XElement("node",
+                            new XAttribute("id", nodeId),
+                            new XAttribute("lat", node.Attributes.GetNamedItem("lat").Value),
+                            new XAttribute("lon", node.Attributes.GetNamedItem("lon").Value));
+                        var targetWay = new XElement("way",
+                            new XAttribute("id", Guid.NewGuid().ToString("N").GetHashCode()),
+                            new XElement("nd",
+                                new XAttribute("ref", nodeId)),
+                            new XElement("tag",
+                                new XAttribute("k", tagKey),
+                                new XAttribute("v", tagValue)));
+                        targetRoot.Add(targetNode);
+                        targetRoot.Add(targetWay);
+                    }
+                }
+
+                var targetDocument = new XDocument(targetRoot);
+                targetDocument.WriteTo(xmlWriter);
+            }
+        }
+    }
+}
---
+++ backend/import/DotNetCoreConverters/PythonMapMatchingInputConverter/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace PythonMapMatchingInputConverter
+{
+    /// <summary>
+    /// Program to read an OpenStreetMap XML and prepare it for the
+    /// <see href="https://github.com/mapillary/map_matching/blob/master/examples/map_matcher.py"/>
+    /// Python library.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine($"Usage: {Process.GetCurrentProcess().MainModule.FileName} {Path.GetFileName(Assembly.GetEntryAssembly().Location)} <path to directory with {FileNameSuffix}{FileNameExtension} files> <path to output directory>");
+                return;
+            }
+
+            // TODO: New converter for removing unneeded tags from XML
+            // TODO: OSM with ways, OSM with nodes only
+
+
+
+
+            var sourceDirectory = new DirectoryInfo(args[0]);
+            var outputDirectory = new DirectoryInfo(args[1]);
+
+            if (sourceDirectory.FullName.Equals(outputDirectory.FullName))
+            {
+                Console.Error.WriteLine("Source and output directory must not be the same");
+                return;
+            }
+
+            if (!outputDirectory.Exists)
+            {
+                outputDirectory.Create();
+            }
+
+            foreach (var sourceFile in sourceDirectory.GetFiles($"*{FileNameSuffix}{FileNameExtension}"))
+            {
+                ProcessFile(sourceFile, outputDirectory);
+            }
+        }
+
+        /// <summary>
+        /// Processes the <paramref name="sourceFile"/>. Generates an output file name and a tag value
+        /// from the input file name. Reads the input content and replaces the existing tags with a
+        /// new tag with key <see cref="TagKey"/>.
+        /// </summary>
+        /// <param name="sourceFile">The source file.</param>
+        /// <param name="outputDirectory">The existing output directory.</param>
+        /// <remarks>
+        /// Done via Regex, since the input files always contain the same pattern, which makes an XML reader
+        /// and writer being too much overhead.
+        /// </remarks>
+        private static void ProcessFile(FileInfo sourceFile, DirectoryInfo outputDirectory)
+        {
+            var tagValue = sourceFile.Name.Replace(FileNameSuffix + FileNameExtension, string.Empty);
+            var outputFile = new FileInfo(Path.Combine(outputDirectory.FullName, tagValue + FileNameExtension));
+
+            using (var readStream = sourceFile.OpenRead())
+            using (var streamReader = new StreamReader(readStream))
+            using (var writeStream = outputFile.OpenWrite())
+            using (var streamWriter = new StreamWriter(writeStream))
+            {
+                var fileContent = streamReader.ReadToEnd();
+
+                var tagXml = new XElement("tag", new XAttribute("k", TagKey), new XAttribute("v", tagValue));
+                var outputContent = TagReplacementRegex.Replace(fileContent, $"\n{tagXml.ToString(SaveOptions.DisableFormatting)}\n");
+
+                streamWriter.Write(outputContent);
+            }
+        }
+    }
+}
