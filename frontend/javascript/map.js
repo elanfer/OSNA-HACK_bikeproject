e454ba56e4c2df1e56ebd9cf88a0ba518cda59ad
Lennard Kollossa <l.kollossa@web.de>
2019-06-28 11:51:12 +0200

    finish color logic

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -3,32 +3,31 @@ var mymap = L.map('mapid').setView([52.281554, 8.042814], 13);
 
 // custom marker iconUrl
 var customMarker = L.icon({
-    iconUrl: 'assets/images/positionSvgator (5).svg',
+  iconUrl: 'assets/images/positionSvgator (5).svg',
 
-    iconSize:     [38, 95], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  iconSize: [38, 95], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
 });
 
 
-function disableLine(color){
-  
+function disableLine(color) {
+
   var paths = document.getElementsByTagName('path')
   var arr = [].slice.call(paths);
-  
+
   arr.map(path => {
-   
     console.log(path.style.stroke)
-    if(path.style.stroke == color){
-      
+    if (path.style.stroke == color) {
+      console.log(path.style.stroke)
       path.style.display = "none";
     }
   })
 }
 
 var active = false;
-function toggle (){
+function toggle() {
   var el = document.getElementById('settings')
-  if(active){
+  if (active) {
     el.style.width = "0px"
     el.style.padding = "0rem;"
     active = false
@@ -40,8 +39,8 @@ function toggle (){
 }
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGVubmFyZHZrIiwiYSI6ImNqeGVvczJlcjBwMjUzb21qdWRtYzdxbjQifQ.QNSNzwAg-_pDSAHbmV-RxA', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-  maxZoom: 50,
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+  maxZoom: 100,
   id: 'mapbox.light-v9',
   accessToken: 'pk.eyJ1IjoibGVubmFyZHZrIiwiYSI6ImNqeGVvczJlcjBwMjUzb21qdWRtYzdxbjQifQ.QNSNzwAg-_pDSAHbmV-RxA'
 }).addTo(mymap);
@@ -51,16 +50,43 @@ this.mymap.locate({
   maxZoom: 140
 }).on("locationfound", e => {
 
-L.marker([e.latitude, e.longitude], {icon: customMarker}).addTo(this.mymap);
+  L.marker([e.latitude, e.longitude], { icon: customMarker }).addTo(this.mymap);
 });
 
+$(document).ready(function () {
 
-
-$( document ).ready(function(){
-
-  $.get("http://10.229.54.121:8080/ways?maxLat=52.277676&minlat=52.279927&maxLng=8.033865&minLng=8.078091", function(){
+  $.get("http://10.229.54.121:8080/ways?maxLat=52.277676&minlat=52.279927&maxLng=8.033865&minLng=8.078091", function () {
     console.log('succes')
   });
+
+  function calcIndex(way) {
+    var street = way.customTags["street"] * 3;
+    var noise = 0;
+    if (way.customTags["noise"] !== undefined) {
+      noise = way.customTags["noise"];
+    }
+    var speed = 0;
+    if (way.customTags["speed_car"] !== undefined) {
+      speed = way.customTags["speed_car"] * 5;
+    }
+    var index = street + noise + speed;
+    index = index / 10;
+    return index
+  }
+
+  function getColorForState(val) {
+
+    var newStateColor
+    if (val <= 0.3) {
+      newStateColor = stateColor = "#DF4848"
+    } else if (val > 0.3 && val <= 0.6) {
+      newStateColor = stateColor = "#FF9C07"
+    } else if (val >= 0.6) {
+      newStateColor = stateColor = "#57C571"
+    }
+    
+    return newStateColor
+  }
 
   function createLines(dataArr) {
 
@@ -70,89 +96,70 @@ $( document ).ready(function(){
       var stateColor = ""
       var indexNum = Math.floor((Math.random() * 10000) + 1);
 
-
-      var objOfNodes = nodes.map(function(obj) {
-        return Object.keys(obj).sort().map(function(key) {
+      var objOfNodes = nodes.map(function (obj) {
+        return Object.keys(obj).sort().map(function (key) {
           return obj[key];
         });
       });
 
-      var arrOfNodes = objOfNodes.map(function(el){
-        var arr=[];
-        for(var key in el){
+      var arrOfNodes = objOfNodes.map(function (el) {
+        var arr = [];
+        for (var key in el) {
           arr.push(el[key]);
         }
         return arr;
       });
 
-      function calcIndex(){
-        var distance = way.normalizedTags * 3
-        var noise = way.normalizedTags * 1
-        var speed = way.normalizedTags * 5
-        var construction = way.normalizedTags * 1
+      var polyline = L.polyline(arrOfNodes, { className: 'my_polyline' + indexNum + '' }).addTo(mymap);
+      var polyLines = document.getElementsByClassName('my_polyline' + indexNum + '');
+      var newStateColor = getColorForState(calcIndex(way, indexNum));
+      polyLines[0].style.stroke = newStateColor;
 
-        var index = distance + noise + speed + construction
-        index = index / 10
-      }
-
-      console.log(way.normalizedTags)
-      var index = 0.1
-      function setPopUpColor(val){
-
-        var polyLine = document.getElementsByClassName('my_polyline' + indexNum + '');
-
-        var newStateColor
-        if (val <= 0.3){
-          newStateColor = stateColor = "#DF4848"
-        }else if(val > 0.3 && val <= 0.6 ) {
-          newStateColor = stateColor = "#FF9C07"
-        }else if(val >= 0.6) {
-          console.log('val', val)
-          newStateColor = stateColor = "#57C571"
-        }
-        console.log(polyLine[0].style.stroke = newStateColor)
-        return newStateColor
-
-
-      }
-
-      var polyline = L.polyline(arrOfNodes,  { className: 'my_polyline' + indexNum + ''}).addTo(mymap);
-
-
-      var polyline = L.polyline(arrOfNodes,  { className: 'my_polyline', id: 'my_polyline' }).addTo(mymap);
       polyline.bindPopup(
-        "<div id='popUp-header'>Urgent</div>"+
-          "<div id='popUp-wrapper' style='background:" + setPopUpColor(index) + "'>"+
-            "<div id='popUp-container-wrapper'>"+
-              "<div id='popUp-container'>"+
-                "<img src=''></img>"+
-                "<div>"+ 'normalizedTags.smoothness' +"</div>"+
-              "</div>"+
-              "<div id='popUp-container'>"+
-                "<img src=''></img>"+
-                "<div>"+ 'normalizedTags.smoothness' +"</div>"+
-              "</div>"+
-            "</div>"+
-          "</div>"
-        +"</div>"
+        "<div id='popUp-header'>Urgent</div>" +
+        "<div id='popUp-wrapper' style='background:" + newStateColor + "'>" +
+        "<div id='popUp-container-wrapper'>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>Straßenname: " + normalizedTags.name + "</div>" +
+        "</div>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>" + normalizedTags.cicleway + "</div>" +
+        "</div>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>" + normalizedTags.bridge + "</div>" +
+        "</div>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>" + normalizedTags.bridge + "</div>" +
+        "</div>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>" + normalizedTags.bridge + "</div>" +
+        "</div>" +
+        "<div id='popUp-container'>" +
+        "<img src=''></img>" +
+        "<div>" + normalizedTags.bridge + "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>"
+        + "</div>"
 
-      ,{
-        showOnMouseOver: true
-      });
-     // mymap.fitBounds(polyline.getBounds());
+        , {
+          showOnMouseOver: true
+        });
+      // mymap.fitBounds(polyline.getBounds());
     });
   }
 
-  mymap.on('moveend', function() { 
-    console.log(mymap.getBounds());
-    $.getJSON("http://10.229.54.121:8080/ways?top=" + mymap.getBounds().getNorth() +"&left="+mymap.getBounds().getWest()+"&bottom="+mymap.getBounds().getSouth()+"&right="+mymap.getBounds().getEast(), function(response){
+  mymap.on('moveend', function () {
+    $.getJSON("http://10.229.54.121:8080/ways?top=" + mymap.getBounds().getNorth() + "&left=" + mymap.getBounds().getWest() + "&bottom=" + mymap.getBounds().getSouth() + "&right=" + mymap.getBounds().getEast(), function (response) {
       createLines(response);
     });
   });
-
 });
-
-
 
 /*fetch('./data.json')
   .then(function(resp) {
