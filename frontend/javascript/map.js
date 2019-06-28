a5a546c28b497bbfdbff02ef178b8e32c417ea7a
Dimitri Schrul <dimitri.schrul@gmail.com>
2019-06-28 14:46:15 +0200

    - declare functions in the right scope - fix flickering of polylines

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -53,112 +53,110 @@ this.mymap.locate({
   L.marker([e.latitude, e.longitude], { icon: customMarker }).addTo(this.mymap);
 });
 
+function calcIndex(way) {
+  return way.customTags["norm_default_calc"];
+}
+
+function getColorForState(val) {
+
+  var newStateColor
+  if (val <= 0.3) {
+    newStateColor = "#DF4848"
+  } else if (val > 0.3 && val <= 0.6) {
+    newStateColor =  "#FF9C07"
+  } else if (val >= 0.6) {
+    newStateColor = "#57C571"
+  }
+
+  return newStateColor
+}
+
+
+function augmentPopup(metric, imagePrefix, ratingPrefixTexts, metricTypeText) {
+  imagePrefix = "assets/images/" + imagePrefix;
+  if (metric != null) {
+    var value = metric;
+    var iconUrl;
+    var ratingPrefixText;
+    if (value < 0.33) {
+      iconUrl = imagePrefix+"Rot.svg";
+      ratingPrefixText = ratingPrefixTexts[0];
+    } else if (value > 0.66) {
+      iconUrl = imagePrefix+"Gruen.svg";
+      ratingPrefixText = ratingPrefixTexts[2];
+    } else {
+      iconUrl = imagePrefix+"Orange.svg";
+      ratingPrefixText = ratingPrefixTexts[1];
+    }
+
+   return  "<div id='popUp-container'>" +
+      "<img class='popUp-container-icon' title='" + value + "' src='" + iconUrl + "'></img>" +
+      "<div>" + ratingPrefixText + " " + metricTypeText + "</div>" +
+      "</div>";
+  }
+  return "";
+}
+
+function createLines(dataArr) {
+
+  dataArr.map(way => {
+    var nodes = way.nodes;
+
+    var objOfNodes = nodes.map(function (obj) {
+      return Object.keys(obj).sort().map(function (key) {
+        return obj[key];
+      });
+    });
+
+    var arrOfNodes = objOfNodes.map(function (el) {
+      var arr = [];
+      for (var key in el) {
+        arr.push(el[key]);
+      }
+      return arr;
+    });
+
+    var newStateColor = getColorForState(calcIndex(way));
+    var polyline = L.polyline(arrOfNodes, { color: newStateColor}).addTo(mymap);
+    
+
+    var popUpHead = "<div id='popUp-header'></div>" +
+      "<div id='popUp-wrapper' style='background:" + newStateColor + "'>" +
+      "<div id='popUp-container-wrapper'>" +
+      "<div id='popUp-container'>" +
+      "<img src=''></img>" +
+      "<div class='street-name'>" + way.osmTags.name + "</div>" +
+      "</div>";
+
+    popUpHead = popUpHead + augmentPopup(way.customTags.norm_street, "Boden", ["schlechter", "mäßiger", "guter"], "Bodenbelag");
+    popUpHead = popUpHead + augmentPopup(way.customTags.norm_speed_car, "Speed", ["schnelle", "mäßig schnelle", "langsame"], "Autos");
+    popUpHead = popUpHead + augmentPopup(way.customTags.norm_construction, "Construction", ["hinderliche", "nicht störende", "keine"], "Baustellen");
+    popUpHead = popUpHead + augmentPopup(way.customTags.norm_noise, "Volume", ["viel", "mäßig", "wenig"], "Lärmbelastung");
+
+    if(way.customTags.userFeedback === 0){
+      popUpHead = popUpHead + "<div id='popUp-container'>" +
+      "<img src='LogoUrbanBikingWeiß.svg'></img>" +
+      "</div>";
+    }
+
+    popUpHead = popUpHead + "</div>" +
+      "</div>" +
+      "</div>";
+
+    polyline.bindPopup(popUpHead, {
+      showOnMouseOver: true
+    });
+    // mymap.fitBounds(polyline.getBounds());
+  });
+}
+
+function updateWays(){
+  $.getJSON("http://10.229.54.121:8080/ways?top=" + mymap.getBounds().getNorth() + "&left=" + mymap.getBounds().getWest() + "&bottom=" + mymap.getBounds().getSouth() + "&right=" + mymap.getBounds().getEast(), function (response) {
+    createLines(response);
+  });
+}
+
 $(document).ready(function () {
-
-
-  function calcIndex(way) {
-    return way.customTags["norm_default_calc"]
-  }
-
-  function getColorForState(val) {
-
-    var newStateColor
-    if (val <= 0.3) {
-      newStateColor = stateColor = "#DF4848"
-    } else if (val > 0.3 && val <= 0.6) {
-      newStateColor = stateColor = "#FF9C07"
-    } else if (val >= 0.6) {
-      newStateColor = stateColor = "#57C571"
-    }
-
-    return newStateColor
-  }
-
-  function augmentPopup(metric, imagePrefix, ratingPrefixTexts, metricTypeText) {
-    imagePrefix = "assets/images/" + imagePrefix;
-    if (metric != null) {
-      var value = metric;
-      var iconUrl;
-      var ratingPrefixText;
-      if (value < 0.33) {
-        iconUrl = imagePrefix+"Rot.svg";
-        ratingPrefixText = ratingPrefixTexts[0];
-      } else if (value > 0.66) {
-        iconUrl = imagePrefix+"Gruen.svg";
-        ratingPrefixText = ratingPrefixTexts[2];
-      } else {
-        iconUrl = imagePrefix+"Orange.svg";
-        ratingPrefixText = ratingPrefixTexts[1];
-      }
-
-     return  "<div id='popUp-container'>" +
-        "<img class='popUp-container-icon' title='" + value + "' src='" + iconUrl + "'></img>" +
-        "<div>" + ratingPrefixText + " " + metricTypeText + "</div>" +
-        "</div>";
-    }
-    return "";
-  }
-
-  function createLines(dataArr) {
-
-    dataArr.map(way => {
-      var nodes = way.nodes;
-      var indexNum = Math.floor((Math.random() * 10000) + 1);
-
-      var objOfNodes = nodes.map(function (obj) {
-        return Object.keys(obj).sort().map(function (key) {
-          return obj[key];
-        });
-      });
-
-      var arrOfNodes = objOfNodes.map(function (el) {
-        var arr = [];
-        for (var key in el) {
-          arr.push(el[key]);
-        }
-        return arr;
-      });
-
-      var polyline = L.polyline(arrOfNodes, { className: 'my_polyline' + indexNum + '' }).addTo(mymap);
-      var polyLines = document.getElementsByClassName('my_polyline' + indexNum + '');
-      var newStateColor = getColorForState(calcIndex(way, indexNum));
-      polyLines[0].style.stroke = newStateColor;
-
-      var popUpHead = "<div id='popUp-header'></div>" +
-        "<div id='popUp-wrapper' style='background:" + newStateColor + "'>" +
-        "<div id='popUp-container-wrapper'>" +
-        "<div id='popUp-container'>" +
-        "<img src=''></img>" +
-        "<div class='street-name'>" + way.osmTags.name + "</div>" +
-        "</div>";
-
-      popUpHead = popUpHead + augmentPopup(way.customTags.norm_street, "Boden", ["schlechter", "mäßiger", "guter"], "Bodenbelag");
-      popUpHead = popUpHead + augmentPopup(way.customTags.norm_speed_car, "Speed", ["schnelle", "mäßig schnelle", "langsame"], "Autos");
-      popUpHead = popUpHead + augmentPopup(way.customTags.norm_construction, "Construction", ["hinderliche", "nicht störende", "keine"], "Baustellen");
-      popUpHead = popUpHead + augmentPopup(way.customTags.norm_noise, "Volume", ["viel", "mäßig", "wenig"], "Lärmbelastung");
-
-      if(way.customTags.userFeedback === 0){
-        popUpHead = popUpHead + "<div id='popUp-container'>" +
-        "<img src='LogoUrbanBikingWeiß.svg'></img>" +
-        "</div>";
-      }
-
-      popUpHead = popUpHead + "</div>" +
-        "</div>" +
-        "</div>";
-
-      polyline.bindPopup(popUpHead, {
-        showOnMouseOver: true
-      });
-      // mymap.fitBounds(polyline.getBounds());
-    });
-  }
-
-  function updateWays(){
-    $.getJSON("http://10.229.54.121:8080/ways?top=" + mymap.getBounds().getNorth() + "&left=" + mymap.getBounds().getWest() + "&bottom=" + mymap.getBounds().getSouth() + "&right=" + mymap.getBounds().getEast(), function (response) {
-      createLines(response);
-    });
-  }
 
   mymap.on('moveend', function () {
     updateWays();
